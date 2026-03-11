530b0507a39e4d15f9eb90fcb63320fe38aef90e
Furkan <furkansln.011@gmail.com>
2026-03-11 03:51:02 +0300

    changed to construction site

---
+++ src/core/constants/services.ts
@@ -0,0 +1,67 @@
+export interface Service {
+  id: string;
+  title: string;
+  description: string;
+  icon: string;
+  slug: string;
+  features: string[];
+}
+
+export const services: Service[] = [
+  {
+    id: '1',
+    title: 'Güçlendirme Çalışmaları',
+    description: 'Binaların deprem yönetmeliğine uygun olarak güçlendirilmesi ve yapısal performans artırımı hizmetleri',
+    icon: 'building',
+    slug: 'guclendirme-calismalari',
+    features: [
+      'Bina Güçlendirme',
+      'Deprem Analizi',
+      'Yapısal Performans Artırımı',
+      'Laboratuvar Testleri',
+      'Proje ve Uygulama',
+    ],
+  },
+  {
+    id: '2',
+    title: 'Su Yalıtım Hizmetleri',
+    description: 'Tüm yapı elemanları için profesyonel su yalıtım çözümleri ve uygulamaları',
+    icon: 'water',
+    slug: 'su-yalitim',
+    features: [
+      'Temel Su Yalıtımı',
+      'Teras ve Balkon Yalıtımı',
+      'Havuz Yalıtımı',
+      'Çatı Yalıtımı',
+      'Perde Duvar Yalıtımı',
+    ],
+  },
+  {
+    id: '3',
+    title: 'Zemin Kaplama Sistemleri',
+    description: 'Endüstriyel ve ticari alanlar için dayanıklı zemin kaplama çözümleri',
+    icon: 'flooring',
+    slug: 'zemin-kaplama',
+    features: [
+      'Epoxy Zemin Kaplama',
+      'Poliüretan Kaplama',
+      'Kendinden Yaylı Zeminler',
+      'Endüstriyel Zemin Çözümleri',
+      'Zemin Tamir ve Bakım',
+    ],
+  },
+  {
+    id: '4',
+    title: 'Yapı Kimyasalları',
+    description: 'Yapı sektöründe kullanılan tüm kimyasal ürünler ve uygulama hizmetleri',
+    icon: 'chemical',
+    slug: 'yapi-kimyasallari',
+    features: [
+      'Silan & Siloksan',
+      'Sızdırmazlık Malzemeleri',
+      'Yapıştırıcılar',
+      'Katkı Maddeleri',
+      'Koruyucu Kimyasallar',
+    ],
+  },
+];
